f683150926d947c0ebd0d8817efded4a224c7da7
Umut M. Dabager <dabager@outlook.com>
2018-05-29 01:07:01 +0300

    New project

---
+++ Warehouse/WarehouseLocation/WarehouseLocation.cpp
@@ -0,0 +1,13 @@
+// WarehouseLocation.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdlib> 
+
+using namespace std;
+
+void main()
+{
+	system("PAUSE");
+}
+
